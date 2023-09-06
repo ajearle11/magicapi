644eba4d09f8c11bd7df13fb71376e9eb28efe0d
ajearle11 <ajearle9@gmail.com>
2023-09-06 16:13:28 +0100

    Created a search route and id route

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ app.get("/", async (req, res) => {
   res.send(cards);
 });
 
+app.get("/search/:name", async (req, res) => {
+  const { name } = req.params;
+  // const card = cards.filter((cardP) => {
+  //   return name.toLowerCase() == cardP.name.toLowerCase();
+  // });
+  const arr = [];
+  const card = cards.forEach((card) => {
+    if (card.name.toLowerCase().includes(name.toLowerCase())) {
+      arr.push(card);
+    }
+  });
+  res.send(arr);
+});
+
+app.get("/id/:id", async (req, res) => {
+  const { id } = req.params;
+  const card = cards.filter((cardP) => {
+    return parseInt(id) == cardP.id;
+  });
+  res.send(card);
+});
+
 module.exports = app;
 
 //How we wrote the jason
