f733927fe5e294dfe6dbac22217483d8ddeb492b
ajearle11 <ajearle9@gmail.com>
2023-09-06 16:32:49 +0100

    Added status codes

--- app.js
+++ app.js
@@ -10,33 +10,56 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.send({
-    message:
-      "Hello. Welcome to the MTG Lords of the Rings: Tales of Middle-Earth LTR card api! You can find images of your favourite cards. /cards will show all the cards. /cards/search/{query} will search by your query. /cards/id/{query} will find a card by its number",
-  });
+  try {
+    res.status(200).send({
+      message:
+        "Hello. Welcome to the MTG Lords of the Rings: Tales of Middle-Earth LTR card api! You can find images of your favourite cards. /cards will show all the cards. /cards/search/{query} will search by your query. /cards/id/{query} will find a card by its number",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get("/cards", async (req, res) => {
-  res.send(cards);
+  try {
+    res.status(200).send(cards);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get("/cards/search/:name", async (req, res) => {
   const { name } = req.params;
   const arr = [];
-  const card = cards.forEach((card) => {
-    if (card.name.toLowerCase().includes(name.toLowerCase())) {
-      arr.push(card);
-    }
-  });
-  res.send(arr);
+  try {
+    const card = cards.forEach((card) => {
+      if (card.name.toLowerCase().includes(name.toLowerCase())) {
+        arr.push(card);
+      }
+    });
+
+    arr.length === 0
+      ? res.status(404).send({ error: "We could not find any cards" })
+      : res.status(200).send(arr);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get("/cards/id/:id", async (req, res) => {
   const { id } = req.params;
-  const card = cards.filter((cardP) => {
-    return parseInt(id) == cardP.id;
-  });
-  res.send(card);
+  try {
+    const card = cards.filter((cardP) => {
+      return parseInt(id) == cardP.id;
+    });
+    card.length === 0
+      ? res
+          .status(404)
+          .send({ error: "We could not find any card with that number" })
+      : res.status(200).send(card);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = app;
